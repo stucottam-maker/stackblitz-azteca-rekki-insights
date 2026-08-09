b5da3d2862a71b9ce89450c6ac60b3b28a6a0cc6
stucottam-maker <stucottam@gmail.com>
2026-08-09 23:50:24 +0100

    removed kitchen

--- app/page.tsx
+++ app/page.tsx
@@ -109,7 +109,7 @@ export default function HomePage() {
             </p>
 
             <h1>
-              Kitchen Insights
+              Insights
             </h1>
 
             <p className="page-description">
