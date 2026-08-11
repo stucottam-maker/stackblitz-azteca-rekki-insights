a10ab963daf53a13511465a82f2a75c65922b673
stucottam-maker <stucottam@gmail.com>
2026-08-11 07:34:24 +0100

    Update supabase.ts

--- app/lib/supabase.ts
+++ app/lib/supabase.ts
@@ -1,6 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.https://owwabaoxadjhkxiijbjp.supabase.co!;
-const supabaseKey = process.env.sb_publishable_rf9sqxZKVb13cAWqEb2_6g_OuvKC12D!;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabasePublishableKey =
+  process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!;
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(
+  supabaseUrl,
+  supabasePublishableKey
+);
