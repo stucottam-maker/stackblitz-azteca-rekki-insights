2ca8fd222e2bb5ae2bd344ae17acc646fc3857f8
stucottam-maker <stucottam@gmail.com>
2026-08-19 11:32:21 +0100

    Polish reports interface

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./mobile-page-fixes.css";
 import "./final-polish.css";
 import "./orders-polish.css";
 import "./login-polish.css";
+import "./reports-polish.css";
 
 import AppFrame from "./components/AppFrame";
 
